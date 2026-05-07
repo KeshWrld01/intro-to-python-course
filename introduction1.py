d21f1dbb3e9af3de4b7ac5a2e57578620aaebd7d
KeshWrld01 <124397226+KeshWrld01@users.noreply.github.com>
2026-05-07 12:17:33 +0300

    Add files via upload

---
+++ introduction1.py
@@ -0,0 +1,20 @@
+print("My name is Victor")
+print("Welcome to python programming")
+
+x = 12
+y = 15
+name = "Khirad"
+age = 14
+print(x)
+print(y)
+print(name)
+print(age)
+print("My name is ", name)
+
+name = "codingal"
+print("\nWelcome to ",name, "\n")
+
+import keyword
+
+print("The following is a list of keywords:")
+print(keyword.kwlist)
